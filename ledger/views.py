b8d818537e1cc53934a0c55c3583b24004542107
jdtcalanno <joshua.dame.calanno@student.ateneo.edu>
2026-03-02 21:42:07 +0800

    Removed redundant Httpresponse import

--- ledger/views.py
+++ ledger/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
