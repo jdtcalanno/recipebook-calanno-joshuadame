275439fd6c9dcc6465c929df9f25b8b9bed798b8
jdtcalanno <joshua.dame.calanno@student.ateneo.edu>
2026-03-02 21:47:39 +0800

    Renamed urls

--- ledger/urls.py
+++ ledger/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import RecipeListView, RecipeDetailView
 
 urlpatterns = [
-    path('recipes/list', RecipeListView.as_view(), name = 'recipeList'),
-    path('recipe/<int:pk>', RecipeDetailView.as_view(), name = 'recipeDetail'),
+    path('recipes/list', RecipeListView.as_view(), name='recipe_list'),
+    path('recipe/<int:pk>', RecipeDetailView.as_view(), name='recipe_detail'),
 ]
 
 app_name = "ledger"
