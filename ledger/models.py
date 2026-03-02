a256c0d4113f995977f0f709053db83f72f55787
jdtcalanno <joshua.dame.calanno@student.ateneo.edu>
2026-03-02 22:00:05 +0800

    Changed recipeList to recipe_list

--- ledger/models.py
+++ ledger/models.py
@@ -41,7 +41,7 @@ class Recipe(models.Model):
         return 'Recipe: {}'.format(self.name)
     
     def get_absolute_url(self):
-        return reverse('ledger:recipeList', args=[str(self.pk)])
+        return reverse('ledger:recipe_list', args=[str(self.pk)])
 
 
 class RecipeIngredient(models.Model):
